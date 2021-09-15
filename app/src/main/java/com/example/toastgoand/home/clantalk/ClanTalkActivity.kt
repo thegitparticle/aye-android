3e3eca3d5fc0fc4c100782fc8f61da2836235382
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-09-15 23:01:31 +0530

    camera for clans done - issue in making cam view disappear

--- app/src/main/java/com/example/toastgoand/home/clantalk/ClanTalkActivity.kt
+++ app/src/main/java/com/example/toastgoand/home/clantalk/ClanTalkActivity.kt
@@ -3,17 +3,19 @@ package com.example.toastgoand.home.clantalk
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.google.accompanist.insets.ui.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.viewbinding.ViewBinding
@@ -21,18 +23,15 @@ import com.example.toastgoand.BaseActivity
 import com.example.toastgoand.composestyle.AyeTheme
 import com.example.toastgoand.databinding.ActivityClanTalkBinding
 import com.example.toastgoand.dummy.DummyClanHub
-import com.example.toastgoand.home.LandingActivity
 import com.example.toastgoand.home.clanframes.ClanFramesActivity
 import com.example.toastgoand.home.clanhub.components.UsersListItem
+import com.example.toastgoand.home.clantalk.camera.CameraActivity
 import com.example.toastgoand.uibits.HeaderPlayScreens
-import com.example.toastgoand.uibits.TopHeaderPlayScreens
 import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.navigationBarsHeight
 import com.google.accompanist.insets.statusBarsPadding
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Camera
 import compose.icons.feathericons.Layers
-import compose.icons.feathericons.PlusCircle
 import compose.icons.feathericons.PlusSquare
 
 class ClanTalkActivity : BaseActivity() {
@@ -41,6 +40,7 @@ class ClanTalkActivity : BaseActivity() {
 
     private lateinit var viewModel: ClanTalkViewModel
 
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = viewBinding as ActivityClanTalkBinding
@@ -58,6 +58,12 @@ class ClanTalkActivity : BaseActivity() {
                 val ongoingFrame = intent.getBooleanExtra("ongoingFrame", false)
                 val startTime = intent.getStringExtra("startTime")
                 val endTime = intent.getStringExtra("endTime")
+
+                var showStartClanOverlay by remember { mutableStateOf(true) }
+
+                if (ongoingFrame) {
+                    showStartClanOverlay = false
+                }
 
                 val context = LocalContext.current
 
@@ -90,52 +96,64 @@ class ClanTalkActivity : BaseActivity() {
                             }
                         },
                         floatingActionButton = {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.Center,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                FloatingActionButton(
-                                    onClick = { /* TODO */ },
-                                    modifier = Modifier
-                                        .padding(horizontal = 25.dp)
-                                        .size(40.dp),
+                            AnimatedVisibility(visible = !showStartClanOverlay) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    FloatingActionButton(
+                                        onClick = { context.startActivity(
+                                            Intent(
+                                                context,
+                                                CameraActivity::class.java
+                                            ).apply {
+                                                putExtra("clubName", clubName)
+                                                putExtra("clubid", clubid)
+                                                putExtra("channelid", channelid)
+                                                putExtra("ongoingFrame", ongoingFrame)
+                                                putExtra("startTime", startTime)
+                                                putExtra("endTime", endTime)
+                                            }) },
+                                        modifier = Modifier
+                                            .padding(horizontal = 25.dp)
+                                            .size(40.dp),
                                         backgroundColor = MaterialTheme.colors.onBackground,
-                                ) {
-                                    Icon(
-                                        imageVector = FeatherIcons.Camera,
-                                        contentDescription = "last month",
-                                        modifier = Modifier.size(20.dp)
-                                    )
-                                }
-                                FloatingActionButton(
-                                    onClick = { /* TODO */ },
-                                    modifier = Modifier
-                                        .padding(horizontal = 25.dp)
-                                        .size(60.dp),
+                                    ) {
+                                        Icon(
+                                            imageVector = FeatherIcons.Camera,
+                                            contentDescription = "last month",
+                                            modifier = Modifier.size(20.dp)
+                                        )
+                                    }
+                                    FloatingActionButton(
+                                        onClick = { /* TODO */ },
+                                        modifier = Modifier
+                                            .padding(horizontal = 25.dp)
+                                            .size(60.dp),
                                         backgroundColor = MaterialTheme.colors.onSurface,
-                                ) {
-                                    Icon(
-                                        imageVector = FeatherIcons.PlusSquare,
-                                        contentDescription = "last month",
-                                        modifier = Modifier.size(30.dp)
-                                    )
-                                }
-                                FloatingActionButton(
-                                    onClick = { /* TODO */ },
-                                    modifier = Modifier
-                                        .padding(horizontal = 25.dp)
-                                        .size(40.dp),
+                                    ) {
+                                        Icon(
+                                            imageVector = FeatherIcons.PlusSquare,
+                                            contentDescription = "last month",
+                                            modifier = Modifier.size(30.dp)
+                                        )
+                                    }
+                                    FloatingActionButton(
+                                        onClick = { /* TODO */ },
+                                        modifier = Modifier
+                                            .padding(horizontal = 25.dp)
+                                            .size(40.dp),
                                         backgroundColor = MaterialTheme.colors.secondary,
-                                ) {
-                                    Icon(
-                                        imageVector = FeatherIcons.Layers,
-                                        contentDescription = "last month",
-                                        modifier = Modifier.size(20.dp)
-                                    )
+                                    ) {
+                                        Icon(
+                                            imageVector = FeatherIcons.Layers,
+                                            contentDescription = "last month",
+                                            modifier = Modifier.size(20.dp)
+                                        )
+                                    }
                                 }
                             }
-
                         },
                         floatingActionButtonPosition = FabPosition.Center,
                     ) {
