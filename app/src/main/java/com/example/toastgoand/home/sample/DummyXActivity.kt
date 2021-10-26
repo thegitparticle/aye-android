0ef453d4a80cc0a02d9528ff977162a1d5b2a517
Set <12975157+thegitparticle@users.noreply.github.com>
2021-10-26 20:00:49 +0530

    dfummy activity added

---
+++ app/src/main/java/com/example/toastgoand/home/sample/DummyXActivity.kt
@@ -0,0 +1,12 @@
+package com.example.toastgoand.home.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.toastgoand.R
+
+class DummyXActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dummy_xactivity)
+    }
+}
