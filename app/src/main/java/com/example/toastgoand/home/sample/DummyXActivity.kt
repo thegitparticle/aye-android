1262f806b4678d79e3aece96eded271a79e28931
Set <12975157+thegitparticle@users.noreply.github.com>
2021-10-26 20:37:14 +0530

    gradle upgrade + base activity of dummy test

--- app/src/main/java/com/example/toastgoand/home/sample/DummyXActivity.kt
+++ app/src/main/java/com/example/toastgoand/home/sample/DummyXActivity.kt
@@ -2,11 +2,22 @@ package com.example.toastgoand.home.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.example.toastgoand.BaseActivity
 import com.example.toastgoand.R
+import com.example.toastgoand.databinding.ActivityDummyXactivityBinding
+import com.example.toastgoand.databinding.ActivityTheAyeBinding
 
-class DummyXActivity : AppCompatActivity() {
+class DummyXActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityDummyXactivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dummy_xactivity)
+        binding = viewBinding as ActivityDummyXactivityBinding
+    }
+
+    override fun binding(): ViewBinding {
+        return ActivityDummyXactivityBinding.inflate(layoutInflater)
     }
 }
