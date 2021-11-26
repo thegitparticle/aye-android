1065c07dc678aca03a0b25674324c3c885deb747
Set <12975157+thegitparticle@users.noreply.github.com>
2021-11-26 17:23:06 +0530

    serialization issues with roomDb objects - fixed 2

--- app/src/main/java/com/example/toastgoand/home/invitepeopledirectly/network/ContactsListItemDataClass.kt
+++ app/src/main/java/com/example/toastgoand/home/invitepeopledirectly/network/ContactsListItemDataClass.kt
@@ -1,5 +1,10 @@
 package com.example.toastgoand.home.invitepeopledirectly.network
 
+import androidx.annotation.Keep
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
 data class ContactsListItemDataClass(
     val name: String,
     val phone: String,
--- app/src/main/java/com/example/toastgoand/network/myfriends/MyFriendsDataClass.kt
+++ app/src/main/java/com/example/toastgoand/network/myfriends/MyFriendsDataClass.kt
@@ -1,12 +1,16 @@
 package com.example.toastgoand.network.myfriends
 
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
 @Entity(
     tableName = "myFriendsTable"
 )
 
+@Keep
+@Serializable
 data class MyFriendsDataClass(
     val name: String,
     @PrimaryKey val friend_user_id: Int,
